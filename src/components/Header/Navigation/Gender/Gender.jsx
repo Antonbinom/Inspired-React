9e6904c350648bb87a70af9a20a2f4dd79fe5c18
Antonbinom <protest_the_hero@mail.ru>
2023-07-08 19:32:20 +0300

    add: isNavLinkActive method

--- src/components/Header/Navigation/Gender/Gender.jsx
+++ src/components/Header/Navigation/Gender/Gender.jsx
@@ -4,17 +4,14 @@ import cn from 'classnames';
 
 const Gender = ({ list }) => {
     const { pathname } = useLocation();
+    const isNavLinkActive = (item) => ({ isActive }) => {
+        return cn(s.link, (isActive || (pathname === '/' && item.link === 'women')) && s.linkActive)
+    }
     return (
         <ul className={s.gender}>
             {list.map(item => (
                 <li className={s.item} key={item.link}>
-                    <NavLink className={
-                        ({ isActive }) => cn(s.link,
-                            ((
-                                isActive
-                                || (pathname === '/' && item.link === 'women'))
-                                && s.linkActive
-                            ))}
+                    <NavLink className={isNavLinkActive(item)}
                         to={item.link}>
                         {item.title}
                     </NavLink>
